f04636a86e318980b16a7f206b05b73c8fadb1c2
Luke Welbourn <luke.tarquin5@outlook.com>
2023-08-08 12:36:31 +0100

    Huge jump, seperated card styles for ease of access and cards now flip correctly, TS emoji Arr created and distributes randomly through created cards.

--- src/main.ts
+++ src/main.ts
@@ -2,30 +2,56 @@ import "./style .css";
 
 const cards = document.querySelectorAll<HTMLElement>(".card");
 const cardContainer = document.querySelector<HTMLElement>(".board-container");
-const startButton = document.querySelector(".start-button");
+const startButton = document.querySelector<HTMLButtonElement>(".start-button");
+const remainingFlips = document.querySelector<HTMLElement>(".remaining-flips");
 
-if (!cards || !startButton || !cardContainer) {
+if (!cards || !startButton || !cardContainer || !remainingFlips) {
   throw new Error("what could possibly go wrong");
 }
+
+// addCardListeners adds event listeners, so changes what happens when a card is pressed.
 
 const addCardListeners = (value: NodeListOf<HTMLElement>) => {
   value.forEach((card) => {
     card.addEventListener("click", () => {
       card.classList.toggle("is-flipped");
-      console.log("we are trying");
+      boardState.flips += 1;
+      console.log(boardState.flips);
+      remainingFlips.innerText = `Flips Remaining ${20 - boardState.flips}`;
     });
   });
 };
 
+// function that creates the cards for each round, must add in template literal for card icons at this stage
+
+const emojiArr: string[] = ["🌟", "🌙", "🌵", "🌸", "🍁", "🍏", "🍒", "🎉"];
+
+const shuffleArr = (value: string[]) => {
+  const arrCopy = value;
+  // Fisher-Yates shuffle to randomize the order
+  for (let i = arrCopy.length - 1; i > 0; i--) {
+    const randomIndex = Math.floor(Math.random() * (i + 1));
+    [arrCopy[i], arrCopy[randomIndex]] = [arrCopy[randomIndex], arrCopy[i]];
+  }
+  return arrCopy;
+};
+
+// console.log(shuffleArr(emojiArr));
+
 const create16Cards = () => {
   cardContainer.innerHTML = "";
+
+  const emojiPairs: string[] = [...emojiArr, ...emojiArr]; // create a long array of 16 emojis (paired)
+  const shuffledEmojiPairs = shuffleArr(emojiPairs);
+
   for (let i = 0; i < 16; i++) {
+    const emoji = shuffledEmojiPairs[i]; // create the cards 1 by 1 in parallel to the paired emoji Arr
     const cardHTML = `<div class="card">
       <div class="card__side card__side--front">
-        ${i + 1}
+      
       </div>
       <div class="card__side card__side--back">
-        back
+      ${emoji}
       </div>
     </div>`;
     const cardElement = document.createElement("div");
@@ -42,4 +68,17 @@ const create16Cards = () => {
   addCardListeners(cards);
 };
 
-startButton.addEventListener("click", create16Cards);
+startButton.addEventListener("click", create16Cards); // code for start button
+
+// good work so far, now for the game state object
+
+type stateType = {
+  gameStart: boolean;
+  cardsFlipped: number;
+  flips: number;
+};
+const boardState: stateType = {
+  gameStart: false,
+  cardsFlipped: 0,
+  flips: 0,
+};
